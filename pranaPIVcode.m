c1019768a3f9c13fb5959baf1bcd9392e15270e5
Matthew Giarra <matthew.giarra@gmail.com>
2012-01-12 10:35:59 -0500

    1) Updated version number in pranaPIVcode.m 2) Changed version number textbox font units from points to pixels

--- pranaPIVcode.m
+++ pranaPIVcode.m
@@ -2,7 +2,7 @@ function varargout=pranaPIVcode(Data)
 
 if ischar(Data)
     if strcmpi(Data,'version')
-        varargout{1} = '2.0.beta.r2011.01.06';
+        varargout{1} = '2.0.beta.r2011.01.11';
     else
         error('Error: String request must be version.')
     end
