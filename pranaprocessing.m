32d8138fd04b7e373e99edf307bfd422c8a94140
Sam Raben <sraben@vt.edu>
2012-08-22 15:29:14 -0400

    Added Direct Cross correlation to the ensemble method.  This required an update to pranaprocessing.m to handle differently shaped correlation planes (planes from DCC are M*N-1 x M*N-1, not simply NxN).  Also the code has been updated to handle odd number windows i.e. 33x33 although these are very slow and probably shouldn't be used.

--- pranaprocessing.m
+++ pranaprocessing.m
@@ -1134,7 +1134,7 @@ switch char(M)
                 end
             end
 
-            Z=[Wsize(e,2),Wsize(e,1),length(X(Eval>=0))];
+            Z=[size(CCm,2), size(CCm,1),length(X(Eval>=0))];
             ZZ=ones(Z(1),Z(2));
 
             if Peakswitch(e) || (Valswitch(e) && extrapeaks(e))
