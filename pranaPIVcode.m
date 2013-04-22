8b1fd7123678f14cce781732762592432d457f02
Sam Raben <sraben@vt.edu>
2013-04-22 11:23:04 -0400

    Created a sub function for building default prana jobs.  Also fixed a bug in the smoothing and updated the version number.

--- pranaPIVcode.m
+++ pranaPIVcode.m
@@ -2,7 +2,7 @@ function varargout=pranaPIVcode(Data)
 
 if ischar(Data)
     if strcmpi(Data,'version')
-        varargout{1} = '2.0.beta.r2013.01.02';
+        varargout{1} = '2.0.beta.r2013.04.22';
     else
         error('Error: String request must be ''version''.')
     end
